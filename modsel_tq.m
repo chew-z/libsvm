422271803c6652beb654a98a2af2c4c0131dfb58
chew-z <chew-z@outlook.com>
2012-09-18 13:31:08 +0200

    Better testing script

--- modsel_tq.m
+++ modsel_tq.m
@@ -1,14 +1,20 @@
-function Z = modsel2(y, x)
+function res = modsel_tq(y, x)
 %
-	best_rmse = 1.0;
+	best_rmse = 1;
+	best_e = 0;
+	best_C = 0;
 	noise = std(y);
+	min_c=1.0;max_c=5;
+	min_e=0.25; max_e=3;
 	i = 1; j = 1;
-	for C=1:0.25:3
-	    for epsilon=0.5:0.25:3
-	        cmd = ['-s 3 -t 0 -v 10 -q -c ',num2str(C*noise),' -p ',num2str(epsilon*noise/sqrt(length(y)))];
+	for C=min_c:0.5:max_c;
+	    for epsilon=min_e:0.25:max_e
+	        cmd = ['-s 3 -t 0 -v 20 -q -c ',num2str(C*noise),' -p ',num2str(epsilon*noise/sqrt(length(y)))]
 	        m = svmtrain(y, x, cmd);
 	        if m < best_rmse 
 	            best_rmse = m;
+	            best_e = epsilon;
+	            best_c = C;
 	        end    
 	        disp(['C = ' num2str(C),' epsilon = ',num2str(epsilon) ' best RMSE = ' num2str(best_rmse)] )
 	        Z(i,j) = m;	% if '-v x' m is rmse otherwise it is model
@@ -17,11 +23,17 @@ function Z = modsel2(y, x)
 	    j = 1;
 	    i = i+1;
 	end
+	res.RMSE = min(min(Z));
+	res.C = best_c;
+	res.epsilon = best_e;
 
-	ylin = linspace(1,3,size(Z,1));
-	xlin = linspace(0.5,3,size(Z,2));
+	res
+
+	% plot 
+	ylin = linspace(min_c,max_c,size(Z,1));
+	xlin = linspace(min_e,max_e,size(Z,2));
 	[X,Y] = meshgrid(xlin,ylin); 
 	mesh(X,Y, Z);
-	xlabel('epsilon'); ylabel('C');zlabel('RMSE');
+	xlabel('epsilon'); ylabel('C');zlabel(['RMSE - stock ' ]);
 	figure(gcf);
 end
--- ms2.m
+++ ms2.m
@@ -1,16 +1,18 @@
-N = 10
+N = 50;
+stock = randi(num_stocks,N,1);
 
-stock = randi(4072,N);
-
-r = zeros(3, N);
+r_linear = zeros(3, N);
 tic
 for i =1:length(stock)
-
 	[x, y] = prepData(alphas, ret1, stock(i), num_alphas, num_dates-1);
-	Z = modsel_tq(double(y),double(x));
-	r(1,i) = max(max(Z));
-	r(2,i) = min(min(Z));
+	if sum(all(x)) > 0	% skip if empty x matrix (means all NaN or no holdings in that stock)
+		res = modsel_tq(double(y),double(x));
+		r_linear(1,i) = res.RMSE;
+		r_linear(2,i) = res.C;
+		r_linear(3,i) = res.epsilon;
+	end
 end
 elapsed = toc
-pushover('koniec', ['Minęło ' num2str(elapsed) ' s.'])
 
+pushover('Koniec', ['Minęło ' num2str(elapsed) ' s.'])
+clear i N res 
